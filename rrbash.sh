bb0f6089c061c4b7dd7462edb4187969384018f3
daniyl-x <daniyl-x@proton.me>
2025-05-23 12:41:28 +0300

    Add arguments to rrbash

--- rrbash.sh
+++ rrbash.sh
@@ -4,8 +4,17 @@
 RBASH_PATH="(mktemp -d)"
 \mkdir "$RBASH_PATH"
 
-# Add clear command
+# Add required commands
 ln -s "$(command -v clear)" "$RBASH_PATH"
+
+# Add all commands passed as arguments to this script
+err=": failed to link"
+for cmd in "$@"; do
+    if [ "$cmd" == "$0" ]; then
+        continue
+    fi
+    ln -s "$(command -v $cmd)" "$RBASH_PATH" &> /dev/null || echo "$cmd$err"
+done
 
 # Remove --norc to get a nice prompt
 (PATH="$RBASH_PATH" "$(command -v bash)" --norc -r)
