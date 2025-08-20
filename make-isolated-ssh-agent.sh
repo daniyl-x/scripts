08402f2638f2d57ac6a6c36e411c20578c803296
daniyl-x <daniyl-x@proton.me>
2025-08-20 19:06:30 +0300

    Fix permissions in ssh-agent scripts

--- make-isolated-ssh-agent.sh
+++ make-isolated-ssh-agent.sh
@@ -4,6 +4,7 @@
 TMP_SSH_AUTH_SOCK_DIR="/tmp/ssh-auth-sock-isolated"
 if [[ ! -d "$TMP_SSH_AUTH_SOCK_DIR" ]]; then
     mkdir "$TMP_SSH_AUTH_SOCK_DIR"
+    chmod 700 "$TMP_SSH_AUTH_SOCK_DIR"
 fi
 
 eval "$(ssh-agent -a "$TMP_SSH_AUTH_SOCK_DIR/$$")" > /dev/null
